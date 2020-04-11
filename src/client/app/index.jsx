28ca9ecba91e252ff476b47b18a83c73d71ed6d4
harry <hjfitz@protonmail.ch>
2020-04-11 19:50:35 +0100

    initial commit with full app. todo: show results after each round

---
+++ src/client/app/index.jsx
@@ -0,0 +1,215 @@
+import {h, render} from 'preact'
+import {useState, useEffect, useRef} from 'preact/hooks'
+import {Router, route} from 'preact-router'
+
+import io from 'socket.io-client'
+
+// todo: Show question after response. give owner opportunity to proceed to next question
+
+
+const App = () => {
+    const [socket, setSocket] = useState(null)
+    const [isOwner, setIsOwner] = useState(null)
+
+    
+
+    // name specifics ------------
+    const [name, setName] = useState(null)
+    const nameEntry = useRef(null)
+    const setUsername = () => {
+        const name = nameEntry.current.value
+        setName(name)
+        route('/set-room')
+    }
+    const keySetUsername = (ev) => {
+        console.log(ev.key)
+        if (ev.key === 'Enter') setUsername()
+    }
+    // -----------------
+
+
+    // room setting specifics -----
+    const [room, setRoom] = useState(null)
+    const [notFound, setNotFound] = useState(false)
+    const roomEntry = useRef(null)
+    //   owner bits
+    const createRoom = () => {
+        console.log('oi')
+        socket.emit('create-room', name)
+        socket.on('room-id', (resp) => {
+            setNotFound(false)
+            console.log('Setting room ID: ', {resp})
+            setRoom(resp)
+            setIsOwner(true)
+            route('/game')
+        })
+    }
+    //   participant bits
+    const joinRoom = () => {
+        const roomName = roomEntry.current.value.toUpperCase()
+        socket.emit('join-room', {name, room: roomName})
+        socket.on('joined', () => {
+            setRoom(roomName)
+            route('/game')
+        })
+    }
+
+    const keyJoinRoom = (ev) => {
+        if (ev.key === 'Enter') joinRoom()
+    }
+    // ------------------
+
+
+    // game bits ---------
+    const [isReady, setIsReady] = useState(false) // ready once we have all participants required
+    const [waiting, setWaiting] = useState(true) // used to grey out buttons
+    const [participants, setParticipants] = useState([]) // list of names
+    const [curQuestion, setCurQuestion] = useState(null)
+
+    const startGame = () => {
+        socket.emit('start-game', room)
+    }
+    
+    
+    const sendColour = (colour) => () => {
+        console.log(colour)
+        socket.emit('response', {room, colour})
+        setWaiting(true)
+    }
+    // -------------------
+
+
+    // response bits ---------
+    const [responses, setResponses] = useState([])
+    // -----------------------
+
+
+    useEffect(() => {
+        if (!name) route('/')
+        const newSocket = io()
+        setSocket(newSocket)
+
+        newSocket.on('new-user', (data) => {
+            console.log(data)
+            setNotFound(false)
+            setParticipants(data)
+        })
+
+        newSocket.on('not-found', () => {
+            setNotFound(true)
+        })
+
+        newSocket.on('question', (question) => {
+            setWaiting(false)
+            console.log(question)
+            setCurQuestion(question)
+        })
+
+        newSocket.on('game-start', () => {
+            console.log('flipping not found')
+            setIsReady(true)
+        })
+
+        newSocket.on('complete', (responses) => {
+            route('/results')
+            console.log({responses})
+            setResponses(responses)
+        })
+    }, [])
+
+
+    return (
+        <main>
+            {name && <p>Welcome, {name}</p>}
+            {room && <p>You are in room {room}</p>}
+            {notFound && <p><strong>Game not found</strong></p>}
+            <Router>
+                <section path="/" className="name">
+                    <label htmlFor="name-input" className="name-label">
+                        Enter Name: 
+                        <input key='name' onKeyUp={keySetUsername}ref={nameEntry} type="text" className="name-input"/>
+                    </label>
+                    <button onClick={setUsername}>Next</button>
+                </section>
+
+
+                <section path="/set-room" className="setroom">
+                        <label htmlFor="setroom-input" className="setroom-label">
+                            Enter a room code: 
+                            <input ref={roomEntry} onKeyUp={keyJoinRoom} key='room' type="text" className="setroom-input"/>
+                        </label>
+                        <button onClick={joinRoom} className="setroom-btn">
+                            Join room
+                        </button>
+                    <section className="startroom">
+                        <p>Or, start your own room</p>
+                        <button onClick={createRoom} className="startroom-start">Start Room</button>
+                    </section>
+                </section>
+
+
+                <section path="/game">
+                        <p>You are {isOwner || 'not'} the room owner. Participants:</p>
+                        <div className="participants">
+                            <ul className="participant-list">
+                                {participants.map(participant => <li key={participant}>{participant}</li>)}
+                            </ul>
+                        </div>
+                        <div className="isready">
+                            {!isReady && <p>Waiting for game to begin</p>}
+                            {(!isReady && isOwner) && <button onClick={startGame} className='isready-readier'>Ready-up</button>}
+                        </div>
+                    <section>
+                        {isReady && (
+                            <div>
+                            {(curQuestion) && (
+                            <div>
+                                <div className="question">
+                                    <h2>{curQuestion.title}</h2>
+                                </div>
+                                <div className="title-descriptions">
+                                    <h3>Green:</h3>
+                                    <p>{curQuestion.green}</p>
+                                    <h3>Red:</h3>
+                                    <p>{curQuestion.red}</p>
+                                </div>
+                            </div>
+                        )}
+                       
+                        {(!isOwner) && (
+                            <div className="responsePanel">
+                                <button disabled={waiting} onClick={sendColour('red')}>Red</button>
+                                <button disabled={waiting} onClick={sendColour('yellow')}>Yellow</button>
+                                <button disabled={waiting} onClick={sendColour('green')}>Green</button>
+                            </div>
+                        )}
+                        </div>
+                        )}       
+                    </section>
+                </section>
+
+
+
+                <section path="/results" className="results">
+                    <h2>Results</h2>
+                    <ul className="results-list">
+                        {responses && responses.map((response) => 
+                            <li className="results-list__result" key={response}>
+                                <h3>{response.title}</h3>
+                                <h4>Green: {response.green}</h4>
+                                <h4>Red: {response.red}</h4>
+                                <p><strong>Red: </strong>{response.responses.red}</p>
+                                <p><strong>Yellow: </strong>{response.responses.yellow}</p>
+                                <p><strong>Green: </strong>{response.responses.green}</p>
+                            </li>
+                        )}
+                    </ul>
+                </section>
+            </Router>
+        </main>
+    )
+}
+
+const entry = document.getElementById('preact')
+
+render(<App />, entry)
---
+++ src/server/index.js
@@ -0,0 +1,208 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const logger = require('morgan')('dev')
+const path = require('path')
+
+const public = path.join(process.cwd(), 'public')
+const idx = path.join(public, 'index.html')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(logger)
+app.use(express.static(public))
+
+app.get('*', (_, res) => res.sendFile(idx))
+
+server.listen(process.env.PORT || 8080, () => {
+    console.log('server listening')
+})
+
+const rooms = {}
+io.on('connection', (socket) => {
+    console.log(socket.id, 'connected')
+    socket.on('create-room', (name) => {
+        console.log('Creating room...')
+        let roomID = createRoomID()
+        while (roomID in rooms) {
+            roomID = createRoomID() // avoid collisions
+        }
+        rooms[roomID] = createRoom(socket.id)
+        rooms[roomID].participants.push({socket, name: `${name} (Owner)`})
+        socket.emit('room-id', roomID)
+        console.log('Created', roomID)
+    })
+
+    socket.on('join-room', (data) => {
+        console.log('request to join', data.room, 'from', data.name)
+        if (data.room in rooms) {
+            const curRoom = rooms[data.room]
+            curRoom.participants.push({socket, name: data.name})
+            curRoom.participants.forEach(user => user.socket.emit('new-user', curRoom.participants.map(user => user.name)))
+            socket.emit('joined')
+            if (curRoom.started) {
+                socket.emit('game-start')
+                socket.emit('question', curRoom.questions[curRoom.questionIndex])
+            }
+        } else {
+            socket.emit('not-found')
+        }
+    })
+
+    socket.on('disconnect', () => {
+        // cleanup rooms
+        Object.entries(rooms).forEach(([id, room]) => {
+            if (room.owner === socket.id) {
+                console.log('cleaning up', id)
+                // inform other participants
+                room.participants.forEach(user => user.socket.emit('game-over'))
+            }
+            // remove that socket from participants and re-emit
+            const newParticipants = room.participants.filter(user => user.socket.id !== socket.id)
+            if (newParticipants.length !== room.participants.length) { // there are differing participants
+                room.participants = newParticipants
+                room.participants.forEach(user => user.socket.emit('new-user', room.participants.map(user => user.name)))
+            }
+        })
+    })
+
+    socket.on('start-game', (room) => {
+        // check to see if socket is room.id
+        if (rooms[room].owner !== socket.id) {
+            console.log('unauthorized start')
+            return
+        }
+
+        // let other sockets know we are starting
+        rooms[room].participants.forEach(({socket}) => socket.emit('game-start'))
+        rooms[room].started = true
+
+        // send first question
+        rooms[room].emitQuestion()
+
+    })
+
+    socket.on('response', ({room, colour}) => {
+        console.log('recieved response', {colour, room})
+        // in room, add response.
+        const curRoom = rooms[room]
+        
+        const responses = curRoom.questions[curRoom.questionIndex].responses
+
+        responses.push(colour)
+
+        // if responses.length === participants.lengt (-1), send a new question
+        if (responses.length === curRoom.participants.length - 1) {
+            // might change to return the responses for each question
+            const isComplete = curRoom.emitQuestion()
+
+            if (!isComplete) return
+        
+            console.log('complete, emitting to all participants')
+            curRoom.participants.forEach(({socket}) => {
+                console.log(curRoom.questions)
+                socket.emit('complete', curRoom.questions.map(question => ({
+                    title: question.title,
+                    green: question.green,
+                    red: question.red,
+                    responses: question.responses.reduce((acc, cur) => {
+                        acc[cur] += 1
+                        return acc
+                    }, {'red': 0, 'yellow': 0, 'green': 0})
+                })))
+            })
+        }
+
+
+    })
+})
+
+const createRoomID = () => Math.random().toString(36).substring(2,6).toUpperCase()
+
+function createRoom(socketID) {
+    return {
+        questionIndex: -1,
+        started: false,
+        owner: socketID,
+        participants: [],
+        emitQuestion() { // handle checking for participant answered in socket.on(response)
+            this.questionIndex += 1
+            if (this.questionIndex >= this.questions.length) return true// ? 'question' : 'complete'
+            this.participants.forEach(({socket}) => {
+                console.log('emitting to socket,', socket.id)
+                socket.emit('question', this.questions[this.questionIndex])
+            })
+            return false
+        },
+        questions: [
+            {
+                title: "Delivering Value",
+                green: "We deliver great stuff! We're proud of it and our stakeholders are really happy. ",
+                red: "We deliver crap. We feel ashamed to deliver it. Our stakeholders hate us. ",
+                responses: []
+            },
+            {
+                title: "Easy to Release",
+                green: "Releasing is simple, safe, painless and mostly automated. ",
+                red: "Releasing is risky, painful, lots of manual work and takes forever. ",
+                responses: []
+            },
+            {
+                title: "Fun",
+                green: "We love going to work and have great fun working together! ",
+                red: "Boooooooring...",
+                responses: []
+            },
+            {
+                title: "Health of Codebase",
+                green: "We're proud of the quality of our code! It is clean, easy to read and has great test coverage. ",
+                red: "Our code is a pile of dung and technical debt is raging out of control. ",
+                responses: []
+            },
+            {
+                title: "Learning",
+                green: "We're learning lots of interesting stuff all the time! ",
+                red: "We never have time to learn anything. ",
+                responses: []
+            },
+            {
+                title: "Mission",
+                green: "We know exactly why we are here and we’re really excited about it! ",
+                red: "We have no idea why we are here, there's no high lever picture or focus. Our so called mission is completely unclear and uninspiring. ",
+                responses: []
+            },
+            {
+                title: "Pawns or Players",
+                green: "We are in control of our own destiny! We decide what to build and how to build it. ",
+                red: "We are just pawns in a game of chess with no influence over what we build or how we build it. ",
+                responses: []
+            },
+            {
+                title: "Speed",
+                green: "We get stuff done really quickly! No waiting and no delays. ",
+                red: "We never seem to get anything done. We keep getting stuck or interrupted. Stories keep getting stuck on dependencies. ",
+                responses: []
+            },
+            {
+                title: "Suitable Process",
+                green: "Our way of working fits us perfectly!",
+                red: "Our way of working sucks!",
+                responses: []
+            },
+            {
+                title: "Support",
+                green: "We always get great support and help when we ask for it! ",
+                red: "We keep getting stuck because we can't get the support and help that we ask for. ",
+                responses: []
+            },
+            {
+                title: "Teamwork",
+                green: "We are a totally gelled super-team with awesome collaboration! ",
+                red: "We are a bunch of individuals that neither know nor care about what the other people in the squad are doing. ",
+                responses: []
+            },
+        ]
+    }
+}
